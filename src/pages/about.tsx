4fb179f9a397c74c703c948d9f96413a42560d92
hyperflow <hyperflow@kakao.com>
2021-03-27 18:09:46 +0900

    fix: Fix about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,8 @@
-import { graphql, useStaticQuery, Link } from 'gatsby';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Seo from '../components/Seo';
 import Layout from '../components/Layout';
 import TextLoop from 'react-text-loop';
 import styled from 'styled-components';
-import oc from 'open-color';
 
 const texts = [
   '프론트엔드 개발자 🚀',
@@ -13,9 +11,13 @@ const texts = [
 ];
 
 const Resume: React.FC = () => {
+  useEffect(() => {
+    console.log("hi");
+  }, [])
+
   return (
     <Layout>
-      <Seo title="About" url="/about" />
+      <Seo title="About" />
       <Introduction>
         <IntroNested>
           안녕하세요?
@@ -41,6 +43,10 @@ const Resume: React.FC = () => {
         <br />
         고려대학교 컴퓨터학과 학부과정 (2019 ~)
         <br />
+        고려대학교 정보대학 5대 학생회 부학생회장 (2020.11 ~)
+        <br />
+        프론트엔드 개발자 @ Riiid, 산업기능요원 복무 중 (2021.04 ~)
+        <br />
         프로그래밍으로 세상을 바꿀 수 있다고 믿습니다.
         <br />
         per aspera, ad astra.
@@ -48,34 +54,14 @@ const Resume: React.FC = () => {
       <Linespace />
       <ResumeTitle>알고리즘과 웹을 좋아합니다.</ResumeTitle>
       <ResumeContext>
-        고려대학교 알고리즘 학회 ALPS에서 회장을 맡고 있습니다. (2020.08~)
+        고려대학교 알고리즘 학회 ALPS에서 회장을 맡았습니다. (2020.08 ~ 2021.02)
         <br />
         요즘은 웹에 관심이 많아 리액트를 공부하면서 프론트엔드의 길을 가고
         있습니다.
         <br />
-        컴퓨터학과 개발팀 ALT_TAB을 구성하여 활동중이며,
+        고려대학교 강의평가 사이트 KLUE에서 프론트엔드 개발자로 참여하고 있습니다.
         <br />
-        <ul style={{ paddingBottom: '10px' }}>
-          <li>
-            <a href="https://github.com/ku-kosmos/anam_food" target="_blank">
-              안암 뭐 먹지? (WebApp w/ React.js)
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/ku-kosmos/my_recipe" target="_blank">
-              나만의 레시피 (Cross-platform App w/ React-native)
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/KU-KUICS/KUICS_frontend"
-              target="_blank"
-            >
-              정보보호학회 KUICS 홈페이지 개편 (w/ React.js)
-            </a>
-          </li>
-        </ul>
-        와 같은 여러 프로젝트를 진행하고 있습니다.
+        또한 컴퓨터학과 개발팀 골방.dev를 구성하여 선순환을 만드는 일을 하고 있습니다.
       </ResumeContext>
       <Linespace />
       <ResumeTitle>이것저것 만드는 것도 좋아합니다.</ResumeTitle>
@@ -91,11 +77,6 @@ const Resume: React.FC = () => {
           <br />
           <ul style={{ paddingBottom: '10px' }}>
             <li>
-              <a href="https://github.com/hyp3rflow/hyperGuess" target="_blank">
-                hyperGuess - 숫자 맞추기 게임 강화학습
-              </a>
-            </li>
-            <li>
               <a
                 href="https://github.com/utilForever/HellSolver"
                 target="_blank"
@@ -104,16 +85,8 @@ const Resume: React.FC = () => {
               </a>
             </li>
             <li>
-              <a
-                href="https://github.com/utilForever/polybridgepp"
-                target="_blank"
-              >
-                Polybridgepp - Poly Bridge 게임 강화학습
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/utilForever/Polisher" target="_blank">
-                Polisher - Cross-Platform 게임 엔진 (w/ Rust)
+              <a href="https://github.com/utilForever/Oxide" target="_blank">
+                Oxide - Cross-Platform 게임 엔진 (w/ Rust)
               </a>
             </li>
           </ul>
